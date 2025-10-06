e1e92faeaf321d0f98be1d2508dd5b3b46faad50
Davi Martins <davi.gurgel@sga.pucminas.br>
2025-10-06 19:00:26 -0300

    Update tp04.java

--- TP's/Tp4/tp04.java
+++ TP's/Tp4/tp04.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 class Game {
     private int id;
